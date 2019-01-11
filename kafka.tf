67703ebe90e9d6294bdd2329e9e08465871d8e59
Thiago Ozores <tozores@gmail.com>
2019-01-11 11:59:07 -0200

    updating project

--- kafka.tf
+++ kafka.tf
@@ -1,11 +1,22 @@
 resource "scaleway_server" "kafka_server" {
   count = 3
   name = "kafka${count.index}"
-  image = "aecaed73-51a5-4439-a127-6d8229847145"
-  type  = "C2S"
-  tags = "kafka"
+  image = "${lookup(var.SCALEWAY_IMAGE, var.SCALEWAY_REGION)}"
+  type  = "START1-S"
+  tags = [ "kafka" ]
 }
 
 resource "scaleway_ip" "kafka_ip" {
   server = "${scaleway_server.kafka_server.id}"
+}
+
+resource "scaleway_volume" "kafka_volume" {
+  name       = "kafka_vol"
+  size_in_gb = 50
+  type       = "l_ssd"
+}
+
+resource "scaleway_volume_attachment" "kafka_vattach" {
+  server = "${scaleway_server.kafka_server.id}"
+  volume = "${scaleway_volume.kafka_volume.id}"
 }
--- vars.tf
+++ vars.tf
@@ -3,3 +3,11 @@ variable "SCALEWAY_ORGID" {}
 variable "SCALEWAY_REGION" {
     default = "par1"
 }
+
+variable "SCALEWAY_IMAGE" {
+  type = "map"
+  default = {
+    par1 = "37832f54-c18f-4338-a552-113e4302a236"
+    ams1 = "e338d2ea-262d-45a1-95d2-300adce63cdd"
+  }
+}
